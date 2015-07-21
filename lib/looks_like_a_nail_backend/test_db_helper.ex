822288b17021013d0f8edf433ad0cc7971a5d68a
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-07-21 18:40:14 +0200

    Added a test DB helper
    To setup more complex DB relationships from test setup.

---
+++ lib/looks_like_a_nail_backend/test_db_helper.ex
@@ -0,0 +1,46 @@
+defmodule LooksLikeANailBackend.TestDBHelper do
+
+  def setup_db(:tool_feature_task) do
+    tool1 = get_property_set("Tool1")
+    tool2 = get_property_set("Tool2")
+    feature1 = get_property_set("Feature1")
+    feature2 = get_property_set("Feature2")
+    task1 = get_property_set("Task1")
+    task2 = get_property_set("Task2")
+    s1 = get_create_statement tool1, feature1, task1
+    s2 = get_create_statement tool2, feature2, task2
+    result = Neo4J.Repo.do_cypher_statements! [s1, s2]
+    data = for result <- result["results"], do: result["data"] |> hd
+    [id1, id2] = for m <- data, do: m["row"] |> hd |> Dict.get("id")
+    {id1, id2}
+  end
+
+  def teardown_db(:tool_feature_task, ids) do
+    statements = for id <- ids, do: get_delete_statement id
+    result = Neo4J.Repo.do_cypher_statements! statements
+  end
+
+  defp get_create_statement(tool, feature, task) do
+    "CREATE (to:Tool #{tool})-[:IMPLEMENTS]->\
+      (f:Feature #{feature})-[:IS_CAPABLE_OF]->\
+      (ta:Task #{task}) \
+      SET to.id = id(to), f.id = id(f), ta.id = id(ta), \
+      to.updated = timestamp(), to.created = timestamp(), \
+      f.updated = timestamp(), f.created = timestamp(), \
+      ta.created = timestamp(), ta.updated = timestamp() \
+      RETURN to, f, ta"
+  end
+
+  defp get_property_set(name) do
+    "{title: \"#{name}\", subTitle: \"Example\", \
+    description: \"Example desc\"}"
+  end
+
+  defp get_delete_statement(id) do
+    "MATCH (tool:Tool {id: #{id}}) \
+      OPTIONAL MATCH (tool)-[r]-(f)-[r2]-(t) \
+      DELETE tool, r, f, r2, t"
+  end
+  
+  
+end
--- test/controllers/tool_controller_test.exs
+++ test/controllers/tool_controller_test.exs
@@ -2,6 +2,7 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
   use LooksLikeANailBackend.ConnCase, async: true
 
   alias LooksLikeANailBackend.Tool
+  alias LooksLikeANailBackend.TestDBHelper
 
   @moduletag :external
 
@@ -10,6 +11,15 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
     keywords: ["key", "word"], insert_at: "", updated_at: ""}
   @invalid_attrs %{}
 
+  setup_all do
+    {id1, id2} = TestDBHelper.setup_db :tool_feature_task
+
+    on_exit fn ->
+      TestDBHelper.teardown_db :tool_feature_task, [id1, id2]
+    end
+    {:ok, id1: id1, id2: id2}
+  end
+
   setup do
     conn = conn() |> put_req_header("accept", "application/json")
     {:ok, conn: conn}
@@ -17,18 +27,18 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, tool_path(conn, :index)
-    # IO.inspect json_response(conn, 200)
-    assert json_response(conn, 200) |> Map.has_key?("tools")
+    response = json_response(conn, 200)
+    assert response |> Map.has_key?("tools")
   end
 
-  test "show one entry", %{conn: conn} do
-    conn = get conn, tool_path(conn, :show, 1)
-    response = json_response(conn, 200)
+  test "show one entry", %{conn: conn, id1: id} do
+    conn = get conn, tool_path(conn, :show, id)
+    IO.inspect response = json_response(conn, 200)
     assert response |> Map.has_key?("tool")
   end
 
   test "try to show one non-existent entry", %{conn: conn} do
-    conn = get conn, tool_path(conn, :show, 999999)
+    conn = get conn, tool_path(conn, :show, 0)
     response = json_response(conn, 404)
     assert response == "Page not found"
   end
@@ -49,30 +59,29 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
   #   # assert json_response(conn, 200) |> Map.has_key?("tools")
   # end
 
-  test "update one entry", %{conn: conn} do
-    entry = %{tool: %{id: 1,
+  test "update one entry", %{conn: conn, id1: id} do
+    entry = %{tool: %{id: id,
       title: "Elixir",
       subTitle: "Programming Language",
-      description: "A functional programming language " <>
-        "standing on the shoulders of giants.",
+      description: "A functional programming language \
+        standing on the shoulders of giants.",
       updated: "2015-07-16T15:46:53.023+0000",
       created: "2015-07-16T15:46:53.023+0000",
       keywords: ["functional", "programming", "erlang"]}}
       # features: [1,2,3]}}
-    conn = put conn, tool_path(conn, :update, 1, entry)
+    conn = put conn, tool_path(conn, :update, id, entry)
     response = json_response(conn, 200)
     assert response |> Map.has_key?("tool")
-    # assert json_response(conn, 200) |> Map.has_key?("tools")
   end
 
-  test "update one partial entry", %{conn: conn} do
-    entry = %{tool: %{id: 1,
-      title: "Elixir",
+  test "update one partial entry", %{conn: conn, id2: id} do
+    entry = %{tool: %{id: id,
+      title: "Java",
       subTitle: "Programming Language",
-      description: "A functional programming language " <>
-        "standing on the shoulders of giants."}}
+      description: "An object oriented programming language"}
+    }
       # features: [1,2,3]}}
-    conn = put conn, tool_path(conn, :update, 1, entry)
+    conn = put conn, tool_path(conn, :update, id, entry)
     response = json_response(conn, 200)
     assert response |> Map.has_key?("tool")
     # assert json_response(conn, 200) |> Map.has_key?("tools")
