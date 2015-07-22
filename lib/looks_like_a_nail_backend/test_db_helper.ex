99c7d0f44a7a93b9c7bacd85ec64211ac80b8d6b
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-07-22 23:14:01 +0200

    Changed existing Tool tests to use testdata.cql

--- lib/looks_like_a_nail_backend/test_db_helper.ex
+++ lib/looks_like_a_nail_backend/test_db_helper.ex
@@ -1,65 +1,8 @@
 defmodule LooksLikeANailBackend.TestDBHelper do
 
   def setup_db(:tool_feature_task) do
-    tool1 = get_property_set("Tool1")
-    tool2 = get_property_set("Tool2")
-    feature1 = get_property_set("Feature1")
-    feature2 = get_property_set("Feature2")
-    task1 = get_property_set("Task1")
-    task2 = get_property_set("Task2")
-    s1 = get_create_statement tool1, feature1, task1
-    s2 = get_create_statement tool2, feature2, task2
-    result = Neo4J.Repo.do_cypher_statements! [s1, s2]
-    data = for result <- result["results"], do: result["data"] |> hd
-    [id1, id2] = for m <- data, do: m["row"] |> hd |> Dict.get("id")
-    {id1, id2}
+    statement = File.read!(Path.expand("~/Projects/js/lookslikeanail-frontend/tests/testdata.cql"))
+    Neo4J.Repo.do_cypher_statements! [statement]
   end
-
-  def teardown_db(:tool_feature_task, ids) do
-    statements = for id <- ids, do: get_delete_statement id
-    result = Neo4J.Repo.do_cypher_statements! statements
-  end
-
-  def create_single_node(type) do
-    statement = "CREATE (n:#{type} {title: \"Example\"}) \
-      SET n.id = id(n) RETURN n"
-    result = Neo4J.Repo.do_cypher_statements! [statement]
-    data = for result <- result["results"], do: result["data"] |> hd
-    ids = for m <- data, do: m["row"] |> hd |> Dict.get("id")
-  end
-
-  def delete_single_node(id) do
-    statement = "MATCH (n {id: #{id}}) DELETE n"
-    result = Neo4J.Repo.do_cypher_statements! [statement]
-  end
-
-  def get_ids_from_neo_response(response) do
-    data = for result <- response["results"], do: result["data"] |> hd
-    ids = for m <- data, do: m["row"] |> hd |> Dict.get("id")
-  end
-  
-
-  defp get_create_statement(tool, feature, task) do
-    "CREATE (to:Tool #{tool})-[:IMPLEMENTS]->\
-      (f:Feature #{feature})-[:IS_CAPABLE_OF]->\
-      (ta:Task #{task}) \
-      SET to.id = id(to), f.id = id(f), ta.id = id(ta), \
-      to.updated = timestamp(), to.created = timestamp(), \
-      f.updated = timestamp(), f.created = timestamp(), \
-      ta.created = timestamp(), ta.updated = timestamp() \
-      RETURN to, f, ta"
-  end
-
-  defp get_property_set(name) do
-    "{title: \"#{name}\", subTitle: \"Example\", \
-    description: \"Example desc\"}"
-  end
-
-  defp get_delete_statement(id) do
-    "MATCH (tool:Tool {id: #{id}}) \
-      OPTIONAL MATCH (tool)-[r]-(f)-[r2]-(t) \
-      DELETE tool, r, f, r2, t"
-  end
-  
-  
+    
 end
--- test/controllers/tool_controller_test.exs
+++ test/controllers/tool_controller_test.exs
@@ -6,18 +6,14 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
 
   @moduletag :external
 
-  @valid_attrs %{description: "some content",
-    title: "title", sub_title: "sub_title",
-    keywords: ["key", "word"], insert_at: "", updated_at: ""}
-  @invalid_attrs %{}
+  # @valid_attrs %{description: "some content",
+  #   title: "title", sub_title: "sub_title",
+  #   keywords: ["key", "word"], insert_at: "", updated_at: ""}
+  # @invalid_attrs %{}
 
   setup_all do
-    {id1, id2} = TestDBHelper.setup_db :tool_feature_task
-
-    on_exit fn ->
-      TestDBHelper.teardown_db :tool_feature_task, [id1, id2]
-    end
-    {:ok, id1: id1, id2: id2}
+    TestDBHelper.setup_db :tool_feature_task
+    :ok
   end
 
   setup do
@@ -31,10 +27,16 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
     assert response |> Map.has_key?("tools")
   end
 
-  test "show one entry", %{conn: conn, id1: id} do
+  test "show one entry", %{conn: conn} do
+    id = 101
+    expected = %{ "tool" => %{"id"=> 101,
+      "title" => "Java", "subTitle" => "Programming Language",
+      "description" => "JavaDescription"}}
     conn = get conn, tool_path(conn, :show, id)
-    IO.inspect response = json_response(conn, 200)
-    assert response |> Map.has_key?("tool")
+    response = json_response(conn, 200)
+    {actual, _} = Dict.split(response["tool"], Dict.keys(expected["tool"]))
+    actual = %{"tool" => actual}
+    assert expected === actual
   end
 
   test "try to show one non-existent entry", %{conn: conn} do
@@ -44,10 +46,10 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
   end
 
   test "create one entry", %{conn: conn} do
-    entry = %{tool: %{title: "Elixir",
+    entry = %{tool: %{title: "Erlang",
       subTitle: "Programming Language",
       description: "A functional programming language " <>
-        "standing on the shoulders of giants.",
+        "built by Joe, Robert and some other guy...",
       updated: "2015-07-16T15:46:53.023+0000",
       created: "2015-07-16T15:46:53.023+0000",
       keywords: ["functional", "programming", "erlang"]}}
@@ -56,38 +58,36 @@ defmodule LooksLikeANailBackend.ToolControllerTest do
     response = json_response(conn, 200)
     assert response |> Map.has_key?("tool")
     id = response |> Map.get("tool") |> Map.get("id")
-    TestDBHelper.delete_single_node(id)
   end
 
-  test "update one entry", %{conn: conn, id1: id} do
+  test "update one entry", %{conn: conn} do
+    id = 104
     entry = %{tool: %{id: id,
       title: "Elixir",
       subTitle: "Programming Language",
-      description: "A functional programming language \
-        standing on the shoulders of giants.",
+      description: "Elixir is a functional, concurrent, general-purpose programming language that runs on the Erlang virtual machine (BEAM). Elixir builds on top of Erlang to provide distributed, fault-tolerant, soft real-time, non-stop applications but also extends it to support metaprogramming with macros and polymorphism via protocols.",
       updated: "2015-07-16T15:46:53.023+0000",
-      created: "2015-07-16T15:46:53.023+0000",
-      keywords: ["functional", "programming", "erlang"]}}
+      created: "2015-07-16T15:46:53.023+0000"}}
       # features: [1,2,3]}}
     conn = put conn, tool_path(conn, :update, id, entry)
     response = json_response(conn, 200)
     assert response |> Map.has_key?("tool")
   end
 
-  test "update one partial entry", %{conn: conn, id2: id} do
+  test "update one partial entry", %{conn: conn} do
+    id = 106
     entry = %{tool: %{id: id,
-      title: "Java",
-      subTitle: "Programming Language",
-      description: "An object oriented programming language"}
+      title: "MySQL",
+      subTitle: "Database",
+      description: "MySQL a free/open source database owned by an evil empire."}
     }
-      # features: [1,2,3]}}
     conn = put conn, tool_path(conn, :update, id, entry)
     response = json_response(conn, 200)
     assert response |> Map.has_key?("tool")
   end
 
   test "delete one entry", %{conn: conn} do
-    id = TestDBHelper.create_single_node("Tool") |> hd
+    id = 106 # MySQL
     conn = delete conn, tool_path(conn, :delete, id)
     response = json_response(conn, 200)
     assert response == %{"tool" => %{}}
