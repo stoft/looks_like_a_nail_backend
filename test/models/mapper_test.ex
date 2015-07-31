04528510b430b779038a9415a680eac47cf7c3e3
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-08-01 01:37:22 +0200

    Completed mapping of neo response for Tool. [ci skip]

--- test/models/mapper_test.ex
+++ test/models/mapper_test.ex
@@ -1,11 +1,11 @@
 defmodule LooksLikeANailBackend.MapperTest do
   use ExUnit.Case, async: true
 
-  alias LooksLikeANailBackend.Feature
+  # alias LooksLikeANailBackend.Feature
   alias LooksLikeANailBackend.Mapper
-  alias LooksLikeANailBackend.Implements
-  alias LooksLikeANailBackend.IsCapableOf
-  alias LooksLikeANailBackend.Task
+  # alias LooksLikeANailBackend.Implements
+  # alias LooksLikeANailBackend.IsCapableOf
+  # alias LooksLikeANailBackend.Task
   alias LooksLikeANailBackend.Tool
 
   @json_one_tool_first_row "{ \"row\": [ { \"title\": \"Elixir\", \"id\": 104, \"created\": 1437522726931, \"updated\": 1438197525156, \"subTitle\": \"Programming Langugage\", \"description\": \"Elixir is a functional...\" }, { \"id\": 3021 }, { \"title\": \"ecto\", \"id\": 302, \"created\": 1437522726931, \"updated\": 1437522726931 }, { \"id\": 3022 }, { \"title\": \"Connecting\", \"id\": 204, \"created\": 1437522726931, \"updated\": 1437522726931, \"subTitle\": \"Task\", \"description\": \"ConnectingDescription\" }, { \"id\": 3024 }, { \"title\": \"MySQL\", \"id\": 106, \"created\": 1437522726931, \"updated\": 1438195241244, \"subTitle\": \"Database\", \"description\": \"MySQL (/maɪ ˌɛskjuːˈɛl/ 'My S-Q-L',[6] officially...\" } ] }"
@@ -45,6 +45,41 @@ defmodule LooksLikeANailBackend.MapperTest do
   #   assert expected === actual
   # end
 
+  test "map tools" do
+    json = "{ \"columns\": [ \"tool\", \"implements\", \"feature\", \"isCapableOf\", \"task\", \"supports\", \"otherTool\" ], \"data\": [ { \"row\": [ { \"title\": \"Java\", \"id\": 101, \"created\": 1437522726931, \"updated\": 1438195114593, \"subTitle\": \"Programming Language\", \"description\": \"Java is a general-purpose computer programming language...\" }, null, null, null, null, null, null ] }, { \"row\": [ { \"title\": \"Elixir\", \"id\": 104, \"created\": 1437522726931, \"updated\": 1438197525156, \"subTitle\": \"Programming Langugage\", \"description\": \"Elixir is a functional, concurrent, general-purpose programming language...\" }, { \"id\": 3021 }, { \"title\": \"ecto\", \"id\": 302, \"created\": 1437522726931, \"updated\": 1437522726931 }, { \"id\": 3022 }, { \"title\": \"Connecting\", \"id\": 204, \"created\": 1437522726931, \"updated\": 1437522726931, \"subTitle\": \"Task\", \"description\": \"ConnectingDescription\" }, { \"id\": 3024 }, { \"title\": \"MySQL\", \"id\": 106, \"created\": 1437522726931, \"updated\": 1438195241244, \"subTitle\": \"Database\", \"description\": \"MySQL (/maɪ ˌɛskjuːˈɛl/ 'My S-Q-L',[6] officially...\" } ] }, { \"row\": [ { \"title\": \"Elixir\", \"id\": 104, \"created\": 1437522726931, \"updated\": 1438197525156, \"subTitle\": \"Programming Langugage\", \"description\": \"Elixir is a functional, concurrent, general-purpose programming language...\" }, { \"id\": 3021 }, { \"title\": \"ecto\", \"id\": 302, \"created\": 1437522726931, \"updated\": 1437522726931 }, { \"id\": 3022 }, { \"title\": \"Connecting\", \"id\": 204, \"created\": 1437522726931, \"updated\": 1437522726931, \"subTitle\": \"Task\", \"description\": \"ConnectingDescription\" }, { \"id\": 3023 }, { \"title\": \"Postgres\", \"id\": 105, \"created\": 1437522726931, \"updated\": 1438194844405, \"subTitle\": \"Database\", \"description\": \"PostgreSQL, often simply Postgres, is an object-relational database...\" } ] }, { \"row\": [ { \"title\": \"Heroku\", \"id\": 107, \"created\": 1437522726931, \"updated\": 1438195289879, \"subTitle\": \"Platform as a Service\", \"description\": \"Heroku is a cloud platform as a service (PaaS)...\" }, { \"id\": 3051 }, { \"title\": \"herokuHosting\", \"id\": 305, \"created\": 1437522726931, \"updated\": 1437522726931 }, { \"id\": 3052 }, { \"title\": \"Hosting\", \"id\": 202, \"created\": 1437522726931, \"updated\": 1437522726931, \"subTitle\": \"Task\", \"description\": \"HostingDescription\" }, { \"id\": 3053 }, { \"title\": \"Postgres\", \"id\": 105, \"created\": 1437522726931, \"updated\": 1438194844405, \"subTitle\": \"Database\", \"description\": \"PostgreSQL, often simply Postgres, is an object-relational database...\" } ] }, { \"row\": [ { \"title\": \"Heroku\", \"id\": 107, \"created\": 1437522726931, \"updated\": 1438195289879, \"subTitle\": \"Platform as a Service\", \"description\": \"Heroku is a cloud platform as a service (PaaS)...\" }, { \"id\": 3051 }, { \"title\": \"herokuHosting\", \"id\": 305, \"created\": 1437522726931, \"updated\": 1437522726931 }, { \"id\": 3052 }, { \"title\": \"Hosting\", \"id\": 202, \"created\": 1437522726931, \"updated\": 1437522726931, \"subTitle\": \"Task\", \"description\": \"HostingDescription\" }, { \"id\": 3054 }, { \"title\": \"Elixir\", \"id\": 104, \"created\": 1437522726931, \"updated\": 1438197525156, \"subTitle\": \"Programming Langugage\", \"description\": \"Elixir is a functional, concurrent, general-purpose programming language...\" } ] } ], \"stats\": { \"contains_updates\": false, \"nodes_created\": 0, \"nodes_deleted\": 0, \"properties_set\": 0, \"relationships_created\": 0, \"relationship_deleted\": 0, \"labels_added\": 0, \"labels_removed\": 0, \"indexes_added\": 0, \"indexes_removed\": 0, \"constraints_added\": 0, \"constraints_removed\": 0 }}"
+    input = Poison.decode!(json)
+    expected = %{tools: [
+        %{title: "Heroku", id: 107, created: 1437522726931, updated: 1438195289879, subTitle: "Platform as a Service", description: "Heroku is a cloud platform as a service (PaaS)...", implements: [3051]},
+        %{id: 104, title: "Elixir", subTitle: "Programming Langugage", updated: 1438197525156, created: 1437522726931, description: "Elixir is a functional, concurrent, general-purpose programming language...", implements: [3021]},
+        %{id: 101, title: "Java", subTitle: "Programming Language", updated: 1438195114593, created: 1437522726931, description: "Java is a general-purpose computer programming language...", implements: []}],
+      implements: [
+        %{id: 3051, feature: 305, tool: 107},
+        %{id: 3021, feature: 302, tool: 104}],
+      features: [
+        %{ title: "herokuHosting", id: 305, created: 1437522726931, updated: 1437522726931, isCapableOf: [3052], supports: [3053, 3054]},
+        %{ title: "ecto", id: 302, created: 1437522726931, updated: 1437522726931, isCapableOf: [3022], supports: [3024,3023]}],
+      isCapableOf: [
+        %{id: 3052, feature: 305, task: 202},
+        %{id: 3022, feature: 302, task: 204}],
+      tasks: [
+        %{ title: "Hosting", id: 202, created: 1437522726931, updated: 1437522726931, subTitle: "Task", description: "HostingDescription"},
+        %{ title: "Connecting", id: 204, created: 1437522726931, updated: 1437522726931, subTitle: "Task", description: "ConnectingDescription" }],
+      supports: [
+        %{id: 3053, feature: 305, tool: 105},
+        %{id: 3054, feature: 305, tool: 104},
+        %{id: 3024, feature: 302, tool: 106},
+        %{id: 3023, feature: 302, tool: 105}]
+    }
+    actual = Mapper.map_entities(Tool, input["data"], input["columns"])
+    assert expected[:tools] === actual[:tools]
+    assert expected[:implements] === actual[:implements]
+    assert expected[:features] === actual[:features]
+    assert expected[:isCapableOf] === actual[:isCapableOf]
+    assert expected[:tasks] === actual[:tasks]
+    assert expected[:supports] === actual[:supports]
+    assert expected[:tools] === actual[:tools]
+  end
+
   test "map one tool two rows" do
     json1 = @json_one_tool_first_row
     json2 = @json_one_tool_second_row
@@ -81,6 +116,15 @@ defmodule LooksLikeANailBackend.MapperTest do
       otherTool: %{id: 106, title: "MySQL", created: 1437522726931, updated: 1438195241244, subTitle: "Database", description: "MySQL (/maɪ ˌɛskjuːˈɛl/ 'My S-Q-L',[6] officially..."}
     }
     actual = Mapper.map_one_row(Tool, row, @columns)
+    assert expected === actual
+  end
+
+  test "map single entity with no features" do
+    json = @json_row_one_tool_no_features
+    row = Poison.decode!(json)
+    expected = %{ tool: %{id: 101, title: "Java", subTitle: "Programming Language", updated: 1438195114593, created: 1437522726931, description: "Java is a general-purpose computer programming language...", implements: []},
+      features: [], implements: [], isCapableOf: [], supports: [], tasks: [], tools: []}
+    actual = Mapper.map_single_entity(Tool, [row], @columns)
     assert expected === actual
   end
 
--- web/models/mapper.ex
+++ web/models/mapper.ex
@@ -1,9 +1,9 @@
 defmodule LooksLikeANailBackend.Mapper do
-  alias LooksLikeANailBackend.Feature
+  # alias LooksLikeANailBackend.Feature
   alias LooksLikeANailBackend.Tool
-  alias LooksLikeANailBackend.Task
-  alias LooksLikeANailBackend.Implements
-  alias LooksLikeANailBackend.IsCapableOf
+  # alias LooksLikeANailBackend.Task
+  # alias LooksLikeANailBackend.Implements
+  # alias LooksLikeANailBackend.IsCapableOf
 
   @types %{ "Feature" => :feature,
             "Tool" => :tool,
@@ -16,77 +16,69 @@ defmodule LooksLikeANailBackend.Mapper do
   @nodes [:feature, :tool, :task, :otherTool]
   @relationships [:implements, :isCapableOf, :supports]
 
-  # @properties [:endNode, :properties, :startNode, :type]
+  def map_entities(Tool, rows, columns) do
+    tools = %{tools: [], implements: [], features: [], isCapableOf: [], tasks: [], supports: []}
+    # rows = for row <- rows, do: map_one_row(Tool, row, columns)
+
+    grouped_rows = group_by_id_of_first_column rows
+    entities = Enum.map(grouped_rows, fn({k, v}) -> map_single_entity(Tool, v, columns) end)
+    tools = Enum.reduce(entities, tools, fn(entity, acc)->
+      acc
+      |> update_in([:tools], &([entity[:tool]] ++ &1))
+      |> update_in([:implements], &(entity[:implements] ++ &1))
+      |> update_in([:features], &(entity[:features] ++ &1))
+      |> update_in([:isCapableOf], &(entity[:isCapableOf] ++ &1))
+      |> update_in([:tasks], &(entity[:tasks] ++ &1))
+      |> update_in([:supports], &(entity[:supports] ++ &1))
+    end)
+    Enum.reduce(tools, %{}, fn({k, v}, acc)-> put_in acc, [k], Enum.uniq(v) end)
+    tools
+  end
+
+  def group_by_id_of_first_column(rows) do
+    rows
+    |> Enum.reduce(%{}, fn(%{"row" => row}, acc)->
+      tool_id = get_in(hd(row), ["id"])
+      Dict.update(acc, tool_id, [%{"row" => row}], &([%{"row" => row}] ++ &1))
+    end)
+    # |> Enum.into([], fn({k, v}) -> v end)
+  end
   
-  # @spec map_from_neo(%{}, atom) :: %{}
-  # def map_from_neo(%{"results" => data}, :get) do
-  #   data = data |> hd |> Dict.get("data")
-  #   mapped_graphs = for graph <- data, do: map_graph(graph)
-  #   unify_graphs(mapped_graphs, :get)
-  # end
-
-  # def map_from_neo(data, :all) do
-    
-  # end
-
-  # @spec map_graph(%{}) :: [...]
-  # def map_graph(graph) do
-  #   relationships = for rel <- graph["relationships"] do
-  #     [type] = for String.capitalize(label) <- rel["type"],
-  #       Enum.member?(@types, label), do: label
-  #     map_entity(to_atom(type), rel)
-  #   end
-
-  #   nodes = for node <- graph["nodes"] do
-  #     [type] = for label <- node["labels"], Enum.member?(@types, label), do: label
-  #     map_entity(to_atom(type), node, relationships)
-  #   end
-  # end
-
-  # @spec unify_graphs([...], atom) :: %{}
-  # def unify_graphs(graphs, :get) do
-  #   Enum.reduce(@types, %{}, fn(type, acc) ->
-  #     list = for entity <- graphs[type], do: entity
-  #     list = Enum.uniq(list)
-  #     case Enum.count(list) do
-  #       1 -> Dict.put(acc, map_name(type, false), hd(list))
-  #       _ -> Dict.put(acc, map_name(type, true), list)
-  #   end)
-  # end
-
-  # @spec map_name(String.t, boolean) :: String.t
-  # defp map_name(name, false), do: String.downcase(name)
-  # defp map_name(name, true) do
-  #   name = String.downcase(name)
-  #   case String.ends_with?(name, "s") do
-  #     true -> name
-  #     false -> name <> "s"
-  #   end
-  # end
+  
 
   def map_single_entity(Tool, rows, columns) do
+    # IO.puts "map_single_entity: "
+    # IO.inspect rows
     rows = for row <- rows, do: map_one_row(Tool, row, columns)
     tool = %{tool: [], implements: [], features: [], isCapableOf: [], tasks: [], supports: [], tools: []}
 
+    # IO.inspect rows
+
     tool = Enum.reduce(rows, tool, fn(row, acc)->
       get_and_put = fn(acc, row, put_key, get_key) ->
-        put_in(acc, [put_key], ([get_in(row, [get_key])] ++ get_in(acc, [put_key]) |> Enum.uniq))
+        if(row_val = get_in(row, [get_key]), do: row_val = [row_val], else: row_val = []) 
+        Dict.update(acc, put_key, row_val, &((row_val ++ &1) |> Enum.uniq))
+        # IO.inspect bar = put_in(acc, [put_key], ([get_in(row, [get_key])] ++ get_in(acc, [put_key]) |> Enum.uniq))
       end
-      acc = get_and_put.(acc, row, :tool, :tool)
-      acc = get_and_put.(acc, row, :implements, :implements)
-      acc = get_and_put.(acc, row, :features, :feature)
-      acc = get_and_put.(acc, row, :isCapableOf, :isCapableOf)
-      acc = get_and_put.(acc, row, :tasks, :task)
-      acc = get_and_put.(acc, row, :supports, :supports)
-      acc = get_and_put.(acc, row, :tools, :otherTool)
+      acc |> get_and_put.(row, :tool, :tool)
+      |> get_and_put.(row, :implements, :implements)
+      |> get_and_put.(row, :features, :feature)
+      |> get_and_put.(row, :isCapableOf, :isCapableOf)
+      |> get_and_put.(row, :tasks, :task)
+      |> get_and_put.(row, :supports, :supports)
+      |> get_and_put.(row, :tools, :otherTool)
     end)
 
-    tool = tool |> put_in([:tool], unify_entity(tool[:tool], [:implements]))
-      |> put_in([:features], unify_entity(tool[:features], [:isCapableOf, :supports]))
-      |> put_in([:tool], hd(get_in(tool,[:tool])))
+    # IO.puts "tool: "
+    # IO.inspect tool
+
+    tool |> put_in([:tool], unify_entity(tool[:tool], [:implements]))
+    |> put_in([:features], unify_entity(tool[:features], [:isCapableOf, :supports]))
+    |> put_in([:tool], hd(get_in(tool,[:tool])))
   end
   
   def unify_entity(entities, fields) do
+    # IO.inspect {entities, fields}
     entity_map = Enum.reduce(fields, %{}, fn(field, acc) ->
       Enum.reduce(entities, acc, fn(entity, acc) ->
         update_entity(acc, entity, field)
@@ -96,15 +88,17 @@ defmodule LooksLikeANailBackend.Mapper do
   end
 
   defp update_entity(entities, entity, field) do
+    # IO.inspect [entities, entity, field]
     if Dict.has_key?(entities, entity[:id]) do
       update_in(entities, [entity[:id], field], &(&1 ++ get_in(entity,[field])|> Enum.uniq))
     else
       put_in(entities, [entity[:id]], entity)
     end
-    # IO.inspect entities
   end
 
   def map_one_row(Tool, %{"row" => row}, columns) do
+    # IO.puts "map_one_row: "
+    # IO.inspect row
     columns = for c <- columns, do: String.to_existing_atom(c)
 
     row = convert_keys_to_atoms row
@@ -170,4 +164,54 @@ defmodule LooksLikeANailBackend.Mapper do
 
   defp convert_keys_to_atoms(simple_value), do: simple_value
   
+  # OLD SHIT
+
+  # @properties [:endNode, :properties, :startNode, :type]
+  
+  # @spec map_from_neo(%{}, atom) :: %{}
+  # def map_from_neo(%{"results" => data}, :get) do
+  #   data = data |> hd |> Dict.get("data")
+  #   mapped_graphs = for graph <- data, do: map_graph(graph)
+  #   unify_graphs(mapped_graphs, :get)
+  # end
+
+  # def map_from_neo(data, :all) do
+    
+  # end
+
+  # @spec map_graph(%{}) :: [...]
+  # def map_graph(graph) do
+  #   relationships = for rel <- graph["relationships"] do
+  #     [type] = for String.capitalize(label) <- rel["type"],
+  #       Enum.member?(@types, label), do: label
+  #     map_entity(to_atom(type), rel)
+  #   end
+
+  #   nodes = for node <- graph["nodes"] do
+  #     [type] = for label <- node["labels"], Enum.member?(@types, label), do: label
+  #     map_entity(to_atom(type), node, relationships)
+  #   end
+  # end
+
+  # @spec unify_graphs([...], atom) :: %{}
+  # def unify_graphs(graphs, :get) do
+  #   Enum.reduce(@types, %{}, fn(type, acc) ->
+  #     list = for entity <- graphs[type], do: entity
+  #     list = Enum.uniq(list)
+  #     case Enum.count(list) do
+  #       1 -> Dict.put(acc, map_name(type, false), hd(list))
+  #       _ -> Dict.put(acc, map_name(type, true), list)
+  #   end)
+  # end
+
+  # @spec map_name(String.t, boolean) :: String.t
+  # defp map_name(name, false), do: String.downcase(name)
+  # defp map_name(name, true) do
+  #   name = String.downcase(name)
+  #   case String.ends_with?(name, "s") do
+  #     true -> name
+  #     false -> name <> "s"
+  #   end
+  # end
+
 end
