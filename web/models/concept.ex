9a826389af6fbbf0bd060642d6a13ae8535ae4b3
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-08-30 10:29:44 +0200

    Implemented Concepts CRUD

--- web/models/concept.ex
+++ web/models/concept.ex
@@ -6,26 +6,26 @@ defmodule LooksLikeANailBackend.Concept do
     subTitle: "",
     keywords: [],
     description: "",
-    inserted: "",
+    created: "",
     updated: "")
 
   @required_fields ~w(id title)
   @optional_fields ~w(subTitle keywords description features)
 
   def get_all_statement() do
-    statement = "MATCH (concept:Concept) RETURN distinct {id: concept.id, title: concept.title, subTitle: concept.subTitle, keywords: concept.keywords, description: concept.description, inserted: concept.inserted, updated: concept.updated}"
+    statement = "MATCH (concept:Concept) RETURN distinct {id: concept.id, title: concept.title, subTitle: concept.subTitle, keywords: concept.keywords, description: concept.description, created: concept.created, updated: concept.updated}"
     parameters = %{}
     {statement, parameters}
   end
 
   def get_get_statement(id) do
-    statement = "MATCH (concept:Concept) WHERE concept.id = {id} RETURN {id: concept.id, title: concept.title, subTitle: concept.subTitle, keywords: concept.keywords, description: concept.description, inserted: concept.inserted, updated: concept.updated}"
+    statement = "MATCH (concept:Concept) WHERE concept.id = {id} RETURN {id: concept.id, title: concept.title, subTitle: concept.subTitle, keywords: concept.keywords, description: concept.description, created: concept.created, updated: concept.updated} as concept"
     parameters = %{id: id}
     {statement, parameters}
   end
 
   def get_delete_statement(id) do
-    statement = "MATCH (t:Concept) WHERE t.id = {id} OPTIONAL MATCH (t)-[r:IMPLEMENTS]->(f:Feature)-[r2:PROVIDES]->(), (f)-[s:SUPPORTS]->() DELETE t, r, f, r2, s"
+    statement = "MATCH (c:Concept) WHERE c.id = {id} OPTIONAL MATCH (c)<-[s:SUPPORTS]-(f:Feature) DELETE c, s"
     parameters = %{id: id}
     {statement, parameters}
   end
@@ -36,23 +36,18 @@ defmodule LooksLikeANailBackend.Concept do
     subTitle = Map.get(concept, "subTitle")
     description = Map.get(concept, "description")
     keywords = Map.get(concept, "keywords")
-    statement = "MATCH (concept:Concept) WHERE concept.id = {id} SET concept.title = {title}, concept.subTitle = {subTitle}, concept.description = {description}, concept.updated = timestamp(), concept.keywords = {keywords} RETURN {id: concept.id, title: concept.title, subTitle: concept.subTitle, description: concept.description, created: concept.created, updated: concept.updated, features: collect(distinct f.id)} as concept"
+    statement = "MATCH (concept:Concept) WHERE concept.id = {id} SET concept.title = {title}, concept.subTitle = {subTitle}, concept.description = {description}, concept.updated = timestamp(), concept.keywords = {keywords} RETURN {id: concept.id, title: concept.title, subTitle: concept.subTitle, description: concept.description, created: concept.created, updated: concept.updated, keywords: concept.keywords} as concept"
     parameters = %{id: id, title: title, subTitle: subTitle, description: description, keywords: keywords}
     {statement, parameters}
   end
 
-  @doc """
-  Generates a create statement from a given Concept.
-
-      iex> LooksLikeANailBackend.Concept.get_create_statement(%{title: "Foo"})
-      "CREATE (concept:Concept {title: "Foo"}) SET concept.id = id(concept) RETURN concept"
-  """
   def get_create_statement(map) do
+    map = Dict.take(map, @required_fields ++ @optional_fields)
     parameters = %{props: map}
     statement = "CREATE (concept:Concept {props}) " <>
     "SET concept.id = id(concept), " <>
     "concept.updated = timestamp(), concept.created = timestamp() " <>
-    "RETURN distinct {id: concept.id, title: concept.title, subTitle: concept.subTitle, keywords: concept.keywords, description: concept.description, inserted: concept.inserted, updated: concept.updated}"
+    "RETURN distinct {id: concept.id, title: concept.title, subTitle: concept.subTitle, keywords: concept.keywords, description: concept.description, created: concept.created, updated: concept.updated} as concept"
     {statement, parameters}
   end
 
